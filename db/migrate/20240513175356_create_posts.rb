2d91953e58a963db1d596f0b88478775a2a2c638
jcmunav63 <jcmunav63@gmail.com>
2024-05-31 10:20:57 -0600

    Destroy Post  scaffolding. Add again remote origin. Edit index file.

--- db/migrate/20240513175356_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[7.1]
-  def change
-    create_table :posts do |t|
-      t.string :post
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_175356) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "post"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
