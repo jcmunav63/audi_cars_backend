7e56fcd4519dd9290f9ef20e50062ff1c0ba3b80
jcmunav63 <jcmunav63@gmail.com>
2024-06-03 23:06:33 -0600

    Add columns image_url & thumbnail to cars table. Migrate changes to cars table. Add urls of three cars.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_181502) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_045130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_181502) do
     t.float "acceleration_sec_to_60mph"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.string "thumbnail"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
